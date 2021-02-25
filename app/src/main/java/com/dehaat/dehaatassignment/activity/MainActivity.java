7aeaf2ea84e6ac40026f304eabc7f0f3f4d95fe9
Pallav Karia <pallav.karia@mountblue.tech>
2021-02-25 20:13:54 +0530

    add recycle view and two pan layout for >400dp width screens

--- app/src/main/java/com/dehaat/dehaatassignment/activity/MainActivity.java
+++ app/src/main/java/com/dehaat/dehaatassignment/activity/MainActivity.java
@@ -8,9 +8,13 @@ import com.dehaat.dehaatassignment.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isTwoPane=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.container_author_books)!=null){
+            isTwoPane=true;
+        }
     }
 }
